c4d682f29429a5ab0826f13dfbe773ab807f841e
caedual <edual986@gmail.com>
2016-06-27 03:00:53 -0300

    pagina de noticias fixes #24

--- ruleta Les Jeux v2.0/js/accion.js
+++ ruleta Les Jeux v2.0/js/accion.js
@@ -2,6 +2,13 @@
 
 function elegirapuesta(){
   var x = document.getElementById("clase-apuesta").value;
+  if (x == "pleno"){
+    var x = document.getElementById("valor-apuesta");
+    var option = document.createElement("option");
+    option.text = "1";
+    x.add(option);
+    alert("bien");
+  }
 
   alert(x);
 
