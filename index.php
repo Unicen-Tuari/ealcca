4920301f14625c931671ea48a0d6c540bfd95b7d
caedual <edual986@gmail.com>
2016-09-30 02:16:45 -0300

    implementacion de SMARTY closes #32

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-  include_once("home.html");
+ require('libs/Smarty.class.php'); //incluimos y evaluamos el archivo smarty
+ $smarty = new Smarty; //creamos una instancia Smarty
+ $smarty->assign("titulo","Maldiito Rock"); //damos valor a las variables a usar en el tpl
+ $smarty->display('home.tpl');
 ?>
