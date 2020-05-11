90d9a47dfb2d0474b9516dfcb0e70b897c170574
Carlos Alcca <edual986@gmail.com>
2020-05-11 04:34:18 -0300

    random desc. and serv.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 "use strict";
 
 
-let sorteo = document.getElementById("btn-sorteo");
-sorteo.addEventListener("click",saludar);
+let btnparticipar = document.getElementById("btn-participar");
+btnparticipar.addEventListener("click",sortear);
 
-function saludar(){
-    document.getElementById("box").style.display = "block";
-}
-
-let close = document.getElementById("btn-close");
-close.addEventListener("click",closebox);
+let btnclose = document.getElementById("btn-close");
+btnclose.addEventListener("click",closebox);
 
 function closebox(){
     document.getElementById("box").style.display = "none";
 }
 
+function sortear(){
+    let descuento = Math.floor(Math.random()*(61-20)+20);
+    let descganado = document.getElementById("desc-ganado");
+    let serviganado = document.getElementById("servi-ganado");
+
+    let servicios = ["Liftin","Ext.Clasic","Ext.Mixta","Ext.Ruso","MakeUp Clasico","MakeUp Artistico"];
+    let servicio = Math.floor(Math.random()*6);
+    let servicioganado = servicios[servicio];
+
+    descganado.innerHTML = descuento;
+    serviganado.innerHTML = servicioganado;
+    document.getElementById("box").style.display = "block";
+    
+}
