9615611a36f3512504e42a4de2b8dca7d7f08954
Carlos Alcca <edual986@gmail.com>
2020-05-12 20:57:52 -0300

    organizing css of the staff

--- js/main.js
+++ js/main.js
@@ -2,26 +2,26 @@
 
 
 let btnparticipar = document.getElementById("btn-participar");
-btnparticipar.addEventListener("click",sortear);
+btnparticipar.addEventListener("click", sortear);
 
 let btnclose = document.getElementById("btn-close");
-btnclose.addEventListener("click",closebox);
+btnclose.addEventListener("click", closebox);
 
-function closebox(){
+function closebox() {
     document.getElementById("box").style.display = "none";
 }
 
-function sortear(){
-    let descuento = Math.floor(Math.random()*(61-20)+20);
+function sortear() {
+    let descuento = Math.floor(Math.random() * (61 - 20) + 20);
     let descganado = document.getElementById("desc-ganado");
     let serviganado = document.getElementById("servi-ganado");
 
-    let servicios = ["Liftin","Ext.Clasic","Ext.Mixta","Ext.Ruso","MakeUp Clasico","MakeUp Artistico"];
-    let servicio = Math.floor(Math.random()*6);
+    let servicios = ["Liftin", "Extensi&oacuten", "Servis", "Perfilado", "Make up clasico", "Make up artistico"];
+    let servicio = Math.floor(Math.random() * 6);
     let servicioganado = servicios[servicio];
 
     descganado.innerHTML = descuento;
     serviganado.innerHTML = servicioganado;
     document.getElementById("box").style.display = "block";
-    
+
 }
